50b1c45b942484a4240316f158adac60c9d0805a
mtborren <mtborren@gmail.com>
2022-11-14 13:25:25 -0500

    added pagination ability to home page list view - commented out in home.html and view.py

--- marvelapp/blog/views.py
+++ marvelapp/blog/views.py
@@ -41,6 +41,7 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-post_date']
+    # paginate_by = 1
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Category.objects.all()
